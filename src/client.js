6662af152ba979c1142ed2d7473e7ab2d55398e9
Søren Vibjerg <vibjerg@gmail.com>
2015-11-11 11:29:38 +0100

    sd-141 new client for entity-suggest web service.

---
+++ src/client.js
@@ -0,0 +1,60 @@
+'use strict';
+
+import request from 'request';
+import {curry} from 'lodash';
+/**
+ * Retrieves data from the webservice based on the parameters given
+ *
+ * @param {Object} params Parameters for the request
+ * @param {string} service
+ * @return {Promise}
+ */
+function sendRequest(logger, uri, qs) {
+  return new Promise((resolve, reject) => {
+    logger.log('suggest client request with params', qs);
+    request.get({uri, qs}, (err, response, body) => {
+      if (err) {
+        logger.error('suggest client responded with an error', {err});
+        reject(err);
+      }
+      else if (response.statusCode !== 200) {
+        logger.error('uri responds with fail statusCode', {path: uri, statusCode: response.statusCode});
+        reject(response);
+      }
+      else {
+        const data = JSON.parse(body);
+        resolve(data);
+        logger.info('suggest client responded with data', {path: uri, params: qs, data: data});
+      }
+    });
+  });
+}
+
+
+/**
+ * Initializes client and return api functions
+ *
+ * @param {Object} config Requires endpoint and port
+ * @returns {{getSubjectSuggestions, getCreatorSuggestions, getLibrarySuggestions}}
+ */
+export function init(config) {
+  if (!config) {
+    throw new Error('no config object provided');
+  }
+
+  if (!config.endpoint) {
+    throw new Error('no endpoint provided in config');
+  }
+
+  if (!config.port) {
+    throw new Error('no port provided in config');
+  }
+  const uri = `${config.endpoint}:${config.port}/${config.method}`;
+  const logger = config.logger || console;
+
+  return {
+    getSubjectSuggestions: curry(sendRequest)(logger)(uri + '/subject'),
+    getCreatorSuggestions: curry(sendRequest)(logger)(uri + '/creator'),
+    getLibrarySuggestions: curry(sendRequest)(logger)(uri + '/library')
+  };
+}
