32eca927006a06f7fd2b5be5480eb41259166b62
Søren Vibjerg <vibjerg@gmail.com>
2015-11-11 11:16:29 +0100

    curry functions

--- src/client.js
+++ src/client.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import request from 'request';
+import {curry} from 'lodash';
 /**
  * Retrieves data from the webservice based on the parameters given
  *
@@ -8,7 +9,7 @@ import request from 'request';
  * @param {string} service
  * @return {Promise}
  */
-function sendRequest(uri, qs, logger = console) {
+function sendRequest(logger, uri, qs) {
   return new Promise((resolve, reject) => {
     logger.log('suggest client request with params', qs);
     request.get({uri, qs}, (err, response, body) => {
@@ -22,7 +23,6 @@ function sendRequest(uri, qs, logger = console) {
       }
       else {
         const data = JSON.parse(body);
-        data.params = qs;
         resolve(data);
         logger.info('suggest client responded with data', {path: uri, params: qs, data: data});
       }
@@ -31,33 +31,7 @@ function sendRequest(uri, qs, logger = console) {
 }
 
 /**
- * Method for requesting Subject entities
- * @param {Object} params Takes the following parameters query, rs, hl, hr
- */
-function getSubjectSuggestions(config, params) {
-  return sendRequest(`${config.uri}/subject`, params, config.logger);
-}
-
-/**
- * Method for requesting Subject entities
- * @param {Object} params Takes the following parameters query, rs, hl, hr
- */
-function getCreatorSuggestions(config, params) {
-  return sendRequest(`${config.uri}/creator`, params, config.logger);
-}
-
-/**
- * Method for requesting Subject entities
- * @param {Object} params Takes the following parameters query, rs, hl, hr
- */
-function getLibrarySuggestions(config, params) {
-  return sendRequest(`${config.uri}/library`, params, config.logger);
-}
-
-/**
- * Setting the necessary paramerters for the client to be usable.
- * The endpoint is only set if endpoint is null to allow setting it through
- * environment variables.
+ * Setting the necessary parameters for the client to be usable.
  *
  * @param {Object} config Config object with the necessary parameters to use
  * the webservice
@@ -74,12 +48,12 @@ export function init(config) {
   if (!config.port) {
     throw new Error('no port provided in config');
   }
-  config.uri = `${config.endpoint}:${config.port}/${config.method}`;
-  config.logger = config.logger || console;
+  const uri = `${config.endpoint}:${config.port}/${config.method}`;
+  const logger = config.logger || console;
 
   return {
-    getSubjectSuggestions: getSubjectSuggestions.bind(null, config),
-    getCreatorSuggestions: getCreatorSuggestions.bind(null, config),
-    getLibrarySuggestions: getLibrarySuggestions.bind(null, config)
+    getSubjectSuggestions: curry(sendRequest)(logger)(uri + '/subject'),
+    getCreatorSuggestions: curry(sendRequest)(logger)(uri + '/creator'),
+    getLibrarySuggestions: curry(sendRequest)(logger)(uri + '/library')
   };
-}
+};
